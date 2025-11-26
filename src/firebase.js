1e42f40575f1ee43c22dc70b3f43d28c75377c2e
Danneal Jocson <dada@Danneals-MacBook-Air.local>
2025-11-26 19:47:14 +0800

    Fixed duplicate import

--- src/firebase.js
+++ src/firebase.js
@@ -3,8 +3,6 @@ import { getFirestore } from "firebase/firestore";
 
 // REPLACE THIS WITH YOUR OWN CONFIG FROM FIREBASE CONSOLE
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
